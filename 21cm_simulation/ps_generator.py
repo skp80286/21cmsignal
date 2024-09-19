997e3484c05294ab8f2712f91db9f421ad05cd64
skp80286 <sameerkpatil@gmail.com>
2024-09-19 10:30:46 +0530

    Zeta and M-min ranges increased as per paper. Generate 10000 samples

--- 21cm_simulation/ps_generator.py
+++ 21cm_simulation/ps_generator.py
@@ -35,6 +35,8 @@ from py21cmfast import cache_tools
 
 from compute_power_spectrum import ComputePowerSpectrum as CPS
 
+import pickle
+
 print(f"Using 21cmFAST version {p21c.__version__}")
 
 if not os.path.exists('_cache'):
@@ -47,27 +49,49 @@ p21c.config['direc'] = '_cache'
 cache_tools.clear_cache(direc="_cache")
 print("Cache cleared")
 
+## Params to be used if using Mass for Zeta
+#user_params = { "HII_DIM": 20, "BOX_LEN": 200, "FAST_FCOLL_TABLES": True, "USE_INTERPOLATION_TABLES": True, 
+#               "N_THREADS": 6, "USE_FFTW_WISDOM": True, "USE_RELATIVE_VELOCITIES": True, 
+#               "POWER_SPECTRUM": 5} # POWER_SPECTRUM: CLASS
+#flag_options = { "USE_MINI_HALOS": True, "M_MIN_in_Mass": True, "USE_MASS_DEPENDENT_ZETA": True, 
+#                "INHOMO_RECO": True, "US_TS_FLUCT": True}
 user_params = { "HII_DIM": 20, "BOX_LEN": 100, "FAST_FCOLL_TABLES": True, "USE_INTERPOLATION_TABLES": True, "N_THREADS": 6, "USE_FFTW_WISDOM": True}
-flag_options = { } #"USE_MINI_HALOS": True}
+flag_options = { }
 
-filename = datetime.now().strftime("output/ps-%Y%m%d%H%M%S.pkl")
-print(filename)
+# File for storing Brightness Temperature maps
+bt_filename = datetime.now().strftime("output/bt-%Y%m%d%H%M%S.pkl")
+print(bt_filename)
 
-zeta_base = 30.0
-zeta_low = zeta_base*0.5  # -50%
-zeta_high = zeta_base*1.5 # +50%
+# File for storing Power Spectra
+ps_filename = datetime.now().strftime("output/ps-%Y%m%d%H%M%S.pkl")
+print(ps_filename)
+
+#zeta_base = 30.0
+#zeta_low = zeta_base*0.5  # -50%
+#zeta_high = zeta_base*1.5 # +50%
+# Following values from paper by chaudhary 2022
+zeta_low = 18 
+zeta_high = 200
 
 m_min_base = math.log10(49999.9995007974)
-m_min_low = m_min_base+math.log10(0.5) # -50%
-m_min_high = m_min_base+math.log10(1.5) # -50%
+m_min_low = m_min_base+math.log10(0.1) # divide by 10
+m_min_high = m_min_base+math.log10(10) # multiply by 10
+
+# Following values for M_min from paper by chaudhary 2022, they are specified as Mass
+# Using these requires change in AstroParams and FlagOptions
+#m_min_low = math.log10(1.09e+09)
+#m_min_high = math.log10(1.19e+11) 
 
 z = 9.1
-nsets = 1000 # number of powerspectra datasets to generate
+nsets = 10000 # number of powerspectra datasets to generate
 
 k_len = -1
 
+print(f'nsets: {nsets}, zeta:[{zeta_low},{zeta_high}], M_min:[{m_min_low},{m_min_high}]')
+
 start_time = time.time()
 for i in range(nsets):
+    if i%100 == 99: print(f'set#{i+1}') 
     zeta = np.random.uniform(zeta_low, zeta_high)
     m_min = np.random.uniform(m_min_low, m_min_high)
     astro_params = {   
@@ -75,6 +99,10 @@ for i in range(nsets):
         "ION_Tvir_MIN": m_min
     }
     coeval = p21c.run_coeval(redshift=9.1, user_params = user_params, astro_params=astro_params, flag_options=flag_options)
+    #print(f'Brightness temp shape {coeval.brightness_temp.shape}')
+    with open(bt_filename, 'a+b') as f:  # open a text file
+        pickle.dump({"zeta": zeta, "m_min": m_min, "bt": coeval.brightness_temp}, f)
+
     #ps = p21mc.Likelihood1DPowerCoeval.compute_power(coeval.brightness_temp, L=100, n_psbins = 10) 
     ps, k = CPS.compute_power_spectrum(user_params["HII_DIM"], coeval.brightness_temp, user_params["BOX_LEN"])
 
@@ -86,6 +114,6 @@ for i in range(nsets):
         print ("Invalid powerspectrum record: skipping...")
         continue
     
-    with open(filename, 'a+b') as f:  # open a text file
+    with open(ps_filename, 'a+b') as f:  # open a text file
         pickle.dump({"zeta": zeta, "m_min": m_min, "ps": ps, "k": k}, f)
 print("--- %s seconds ---" % (time.time() - start_time))
